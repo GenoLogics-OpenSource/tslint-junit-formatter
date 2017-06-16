e14f85a12adc48cf803b7a518438fe259cd866a3
Chris Smith <evilangelist42@gmail.com>
2017-06-16 10:09:52 -0700

    Updated to support TSLint 5.x (#5)

--- formatters/junitFormatter.ts
+++ formatters/junitFormatter.ts
@@ -1,5 +1,4 @@
-import * as ts from "typescript";
-import * as Lint from "tslint/lib/lint";
+import * as Lint from "tslint";
 
 /*
  * TSLint formatter that adheres to the JUnit XML specification.
@@ -33,29 +32,29 @@ export class Formatter extends Lint.Formatters.AbstractFormatter {
      * Generate an error <testcase> element
      */
     private testcaseXML(ruleFailure: Lint.RuleFailure): string {
-        var ruleName = this.generateName(ruleFailure.getRuleName());
-        var message = this.escape(ruleFailure.getFailure());
-        var startPosition = ruleFailure.getStartPosition();
-        var { line, character } = startPosition.getLineAndCharacter();
-        var fileName = ruleFailure.getFileName();
+        const ruleName = this.generateName(ruleFailure.getRuleName());
+        const message = this.escape(ruleFailure.getFailure());
+        const startPosition = ruleFailure.getStartPosition();
+        const { line, character } = startPosition.getLineAndCharacter();
+        const fileName = ruleFailure.getFileName();
 
-        return `<testcase time=\"0\" name=\"${ruleName}\"><error message=\"${message} (${ruleName})\"><![CDATA[${line}:${character}:${fileName}]]></error></testcase>`;
+        return `<testcase time="0" name="${ruleName}"><error message="${message} (${ruleName})"><![CDATA[${line}:${character}:${fileName}]]></error></testcase>`;
     }
 
     /**
      * Generate a <testsuite> element without a closing tag
      */
     private testsuiteStartXML(failures: Lint.RuleFailure[]): string {
-        return `<testsuite time=\"0\" tests=\"${failures.length}\" skipped=\"0\" errors=\"${failures.length}\" failures=\"0\" package=\"org.tslint\" name=\"tslint.xml\">`;
+        return `<testsuite time="0" tests="${failures.length}" skipped="0" errors="${failures.length}" failures="0" package="org.tslint" name="tslint.xml">`;
     }
 
     /**
      * Transform lint failure to JUnit XML format
      */
     public format(failures: Lint.RuleFailure[]): string {
-        var xml = [];
+        let xml = [];
 
-        xml.push("<?xml version=\"1.0\" encoding=\"utf-8\"?>");
+        xml.push(`<?xml version="1.0" encoding="utf-8"?>`);
         xml.push("<testsuites>");
 
         if (failures.length > 0) {
